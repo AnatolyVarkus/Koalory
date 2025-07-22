d7035dab483a585eb2f92ff0a82e0fbcd6df0e71
Anatoly <anatoly@Anatolys-MacBook-Pro.local>
2025-07-22 13:21:45 -0400

    asdasd

--- app/schemas/payment_schema.py
+++ app/schemas/payment_schema.py
@@ -2,3 +2,7 @@ from pydantic import BaseModel
 
 class PaymentResponse(BaseModel):
     link: str
+
+class PaymentRequest(BaseModel):
+    option: str
+    job_id: int | None = None
--- app/services/stripe_service.py
+++ app/services/stripe_service.py
@@ -3,7 +3,11 @@ from app.core import settings, variables
 
 stripe.api_key = settings.STRIPE_API_KEY
 
-def create_stripe_payment_link(user_id: int, job_id: int, option: str) -> str:
+def create_stripe_payment_link(user_id: int, job_id: int | None, option: str) -> str:
+    if job_id is None:
+        success_url = f"https://story.koalory.com/payment?success=true"
+    else:
+        success_url = f"https://story.koalory.com/payment?success=true&job_id={job_id}"
     session = stripe.checkout.Session.create(
         payment_method_types=["card"],
         mode="payment",
@@ -22,7 +26,7 @@ def create_stripe_payment_link(user_id: int, job_id: int, option: str) -> str:
         metadata={
             "user_id": int(user_id)
         },
-        success_url=f"https://story.koalory.com/success?job_id={job_id}",
-        cancel_url="https://yourdomain.com/cancel"
+        success_url=success_url,
+        cancel_url="https://story.koalory.com/payment?success=false"
     )
     return session.url
