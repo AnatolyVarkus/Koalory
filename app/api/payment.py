72f2795b6c77b62e1aa1edb77811575e6fac8ccf
Anatoly <anatoly.varkus@gmail.com>
2025-07-22 13:21:45 -0400

    asdasd

--- app/api/payment.py
+++ app/api/payment.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, Form, File, UploadFile, HTTPException
 from fastapi.responses import Response
 from app.core.wrapper import CustomRoute
-from app.schemas.payment_schema import (PaymentResponse)
+from app.schemas.payment_schema import (PaymentResponse, PaymentRequest)
 from app.services import jwt_service, form_handler_service, gcs_uploader
 from app.db import AsyncSessionLocal
 from sqlalchemy import select, and_
@@ -12,19 +12,20 @@ from app.models import StoriesModel
 from PIL import Image
 from io import BytesIO
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from app.services import create_stripe_payment_link
 
 auth_scheme = HTTPBearer()
 router = APIRouter(prefix="/payment", route_class=CustomRoute)
 
 
 @router.post("/generate_payment_link")
-async def generate_payment_link(option: str = Form(...), credentials: HTTPAuthorizationCredentials = Depends(auth_scheme)) -> PaymentResponse:
-    print(f"{option = }")
+async def generate_payment_link(payload: PaymentRequest,
+                                credentials: HTTPAuthorizationCredentials = Depends(auth_scheme)) -> PaymentResponse:
+    print(f"{payload.option = }")
     if credentials.scheme.lower() != "bearer":
         raise HTTPException(status_code=401, detail="Invalid authentication scheme")
-    payload = jwt_service.decode_jwt(credentials.credentials)
-    user_id = payload.get("sub")
+    jwt_payload = jwt_service.decode_jwt(credentials.credentials)
+    user_id = jwt_payload.get("sub")
 
-
-    payment_url = f"https://payment.example.com/checkout?user_id={user_id}&option={option}"
-    return PaymentResponse(link=payment_url)
+    link = create_stripe_payment_link(user_id, payload.job_id, payload.option)
+    return PaymentResponse(link=link)
--- app/services/__init__.py
+++ app/services/__init__.py
@@ -7,3 +7,4 @@ from .ai_request_service import external_request_handler, InternalRequest
 from .ai_photo_generation import AIPhotoGenerator
 from .google_storage_service import gcs_uploader
 from .ai_photo_analysis import GPTVisionClient
+from .stripe_service import create_stripe_payment_link
