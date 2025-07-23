6a873c4b25004782b247fa5d5a3aef391123c1f7
Anatoly <anatoly@Anatolys-MacBook-Pro.local>
2025-07-23 14:19:56 -0400

    asdasd

--- app/db/db_functions.py
+++ app/db/db_functions.py
@@ -1,7 +1,7 @@
 from .database import AsyncSessionLocal
 from sqlalchemy import select
 
-async def db_add(obj: object, db: AsyncSessionLocal):
+async def db_add(obj: object, db):
     try:
         db.add(obj)
         await db.commit()
@@ -9,21 +9,21 @@ async def db_add(obj: object, db: AsyncSessionLocal):
     except Exception:
         raise
 
-async def get_user_by_email(email: str, db: AsyncSessionLocal):
+async def get_user_by_email(email: str, db):
     from app.models import UsersModel
     stmt = select(UsersModel).where(UsersModel.email == email)
     result = await db.execute(stmt)
     user = result.scalar_one_or_none()
     return user
 
-async def check_user(user_id: int, db: AsyncSessionLocal):
+async def check_user(user_id: int, db):
     from app.models import UsersModel
     stmt = select(UsersModel).where(UsersModel.id == int(user_id))
     result = await db.execute(stmt)
     user = result.scalar_one_or_none()
     return user
 
-async def get_story_by_job_id(job_id: int, db: AsyncSessionLocal):
+async def get_story_by_job_id(job_id: int, db):
     from app.models import StoriesModel
     stmt = select(StoriesModel).where(StoriesModel.id == int(job_id))
     result = await db.execute(stmt)
@@ -33,7 +33,7 @@ async def get_story_by_job_id(job_id: int, db: AsyncSessionLocal):
 async def check_story_ownership(
     user_id: int,
     job_id: int,
-    db: AsyncSessionLocal
+    db
 ) -> bool:
     from app.models import StoriesModel
     stmt = select(StoriesModel).where(
@@ -44,7 +44,7 @@ async def check_story_ownership(
     story = result.scalar_one_or_none()
     return story is not None
 
-async def get_all_user_stories(user_id: int, session: AsyncSessionLocal):
+async def get_all_user_stories(user_id: int, session):
     from app.models import StoriesModel
     stmt = select(StoriesModel).where(StoriesModel.user_id == int(user_id))
     result = await session.execute(stmt)
