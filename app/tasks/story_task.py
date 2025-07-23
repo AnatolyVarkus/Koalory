8e56c33b3bb4b9491173b6d11a5c25250b2b4241
Anatoly <anatoly@Anatolys-MacBook-Pro.local>
2025-07-23 14:24:04 -0400

    asdasd

--- app/tasks/story_task.py
+++ app/tasks/story_task.py
@@ -1,10 +1,10 @@
 from celery import shared_task
 from app.services.ai_story_generation import StoryGeneratorService
-import asyncio
+from anyio import run as anyio_run
 
 @shared_task
 def run_story_generation(user_id: int, job_id: int):
-    asyncio.run(_run(user_id, job_id))
+    anyio_run(_run, user_id, job_id)
 
 async def _run(user_id: int, job_id: int):
     generator = StoryGeneratorService(job_id, user_id)
