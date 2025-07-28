1e4d289b8f3788570680d4dafe1f9e1bd5c54091
Anatoly <anatoly.varkus@gmail.com>
2025-07-27 22:51:28 -0400

    asdsad

--- app/services/email_sender_service.py
+++ app/services/email_sender_service.py
@@ -8,24 +8,22 @@ resend.api_key = settings.RESEND_API_KEY
 
 #
 async def send_email_code(to_email: str, code: str):
+    print(f"Code: {code}")
     r = resend.Emails.send(resend.Emails.SendParams(**{
-        "from": "Acme <onboarding@resend.dev>",
+        "from": "Koalory <onboarding@resend.dev>",
         "to": [to_email],
         "subject": "Email Verification Code",
         "html": f"<strong>Your code is: {code}</strong>"}))
 
-async def send_reset_email(to_email: str, token: str):
+def send_reset_email(to_email: str, token: str):
     url = f"https://story.koalory.com/auth/reset?reset_token={token}"
+    print(f"Token: {token}")
+    print(f"URL: {url}")
 
-    params: resend.Emails.SendParams = {
-        "from": "Acme <onboarding@resend.dev>",
+    params = resend.Emails.SendParams(**{
+        "from": "Koalory <onboarding@resend.dev>",
         "to": [to_email],
         "subject": "Reset Your Password",
-        "html": f"<p>Click <a href=\"{url}\">here</a> to reset your password. This link expires in 1 hour.</p>"}
-    try:
-        r = resend.Emails.send(params)
-
-    except Exception as e:
-        print(f"Exception: {e}")
-
+        "html": f"<p>Click <a href=\"{url}\">here</a> to reset your password. This link expires in 1 hour.</p>"})
+    r = resend.Emails.send(params)
 
