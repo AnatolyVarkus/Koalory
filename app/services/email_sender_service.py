2bb304dc47a5300f73b49a63da031f2969568ffe
Anatoly <anatoly.varkus@gmail.com>
2025-07-27 22:41:51 -0400

    adads

--- app/services/email_sender_service.py
+++ app/services/email_sender_service.py
@@ -7,12 +7,12 @@ from app.core.config import settings
 resend.api_key = settings.RESEND_API_KEY
 
 #
-# async def send_email_code(to_email: str, code: str):
-#     r = resend.Emails.send(resend.Emails.SendParams(**{
-#         "from": "Acme <onboarding@resend.dev>",
-#         "to": [to_email],
-#         "subject": "Email Verification Code",
-#         "html": f"<strong>Your code is: {code}</strong>"}))
+async def send_email_code(to_email: str, code: str):
+    r = resend.Emails.send(resend.Emails.SendParams(**{
+        "from": "Acme <onboarding@resend.dev>",
+        "to": [to_email],
+        "subject": "Email Verification Code",
+        "html": f"<strong>Your code is: {code}</strong>"}))
 
 async def send_reset_email(to_email: str, token: str):
     url = f"https://story.koalory.com/auth/reset?reset_token={token}"
