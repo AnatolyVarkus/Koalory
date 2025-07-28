4c281f794c3b2dfd52dd108f40f19a8aafff2e9a
Anatoly <anatoly.varkus@gmail.com>
2025-07-27 22:41:19 -0400

    asdads

--- app/services/email_sender_service.py
+++ app/services/email_sender_service.py
@@ -4,17 +4,15 @@ from sendgrid.helpers.mail import Mail
 import os
 from app.core.config import settings
 
-print(f"settings.RESEND_API_KEY: {settings.RESEND_API_KEY}")
 resend.api_key = settings.RESEND_API_KEY
-print(f"resend.api_key: {resend.api_key}")
 
-
-async def send_email_code(to_email: str, code: str):
-    r = resend.Emails.send(resend.Emails.SendParams(**{
-        "from": "Acme <onboarding@resend.dev>",
-        "to": [to_email],
-        "subject": "Email Verification Code",
-        "html": f"<strong>Your code is: {code}</strong>"}))
+#
+# async def send_email_code(to_email: str, code: str):
+#     r = resend.Emails.send(resend.Emails.SendParams(**{
+#         "from": "Acme <onboarding@resend.dev>",
+#         "to": [to_email],
+#         "subject": "Email Verification Code",
+#         "html": f"<strong>Your code is: {code}</strong>"}))
 
 async def send_reset_email(to_email: str, token: str):
     url = f"https://story.koalory.com/auth/reset?reset_token={token}"
@@ -26,6 +24,7 @@ async def send_reset_email(to_email: str, token: str):
         "html": f"<p>Click <a href=\"{url}\">here</a> to reset your password. This link expires in 1 hour.</p>"}
     try:
         r = resend.Emails.send(params)
+
     except Exception as e:
         print(f"Exception: {e}")
 
