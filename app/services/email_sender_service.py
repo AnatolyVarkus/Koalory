0a8602958b3eb295c23589b25a8a2736db0d8460
Anatoly <anatoly.varkus@gmail.com>
2025-07-27 22:35:19 -0400

    asdads

--- app/services/email_sender_service.py
+++ app/services/email_sender_service.py
@@ -12,18 +12,19 @@ print(f"resend.api_key: {resend.api_key}")
 async def send_email_code(to_email: str, code: str):
     r = resend.Emails.send(resend.Emails.SendParams(**{
         "from": "Acme <onboarding@resend.dev>",
-        "to": to_email,
+        "to": [to_email],
         "subject": "Email Verification Code",
         "html": f"<strong>Your code is: {code}</strong>"}))
 
 async def send_reset_email(to_email: str, token: str):
     url = f"https://story.koalory.com/auth/reset?reset_token={token}"
 
-    r = resend.Emails.send(resend.Emails.SendParams(**{
+    params: resend.Emails.SendParams = {
         "from": "Acme <onboarding@resend.dev>",
-        "to": to_email,
+        "to": [to_email],
         "subject": "Reset Your Password",
         "html": f"<p>Click <a href=\"{url}\">here</a> to reset your password. This link expires in 1 hour.</p>"}
-    ))
+
+    r = resend.Emails.send(params)
 
 
