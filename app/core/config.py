b9649d1c4f7f8eca67b31e46dcefc3070d3b8b6c
Anatoly <anatoly.varkus@gmail.com>
2025-07-29 19:48:55 -0400

    asdsad

--- app/core/config.py
+++ app/core/config.py
@@ -4,12 +4,8 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-
-
-# import os
-# print("GOOGLE_APPLICATION_CREDENTIALS =", os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
-# "/Users/anatoly/Important/PycharmProjects/Koalory/app/koalory_google.json"
-# "/home/koalory_bot/Koalory/app/koalory_google.json"
+def str_to_bool(value: str) -> bool:
+    return value.lower() in ("true", "1", "yes")
 
 class Settings(BaseModel):
     DEBUG: bool = False
@@ -20,14 +16,19 @@ class Settings(BaseModel):
     LEONARDO_MODEL_ID: str = "de7d3faf-762f-48e0-b3b7-9d0ac3a3fcf3"
     BUCKET_NAME: str = "koalory_bucket"
     API_KEY_EXTERNAL: str = os.environ.get("API_KEY_EXTERNAL")
-    STRIPE_API_KEY: str = os.environ.get("STRIPE_API_KEY")
     LEONARDO_API_KEY: str = os.environ.get("LEONARDO_API_KEY")
     ANTHROPIC_API_KEY: str = os.environ.get("ANTHROPIC_API_KEY")
     GPT_API_KEY: str = os.environ.get("GPT_API_KEY")
     CELERY_BROKER_URL: str = "redis://localhost:6379/0"
     CELERY_RESULT_BACKEND: str ="redis://localhost:6379/1"
-    STRIPE_WEBHOOK_SECRET: str = os.environ.get("STRIPE_WEBHOOK_SECRET")
     RESEND_API_KEY: str = os.environ.get("RESEND_API_KEY")
+    LIVE_SERVER: bool = str_to_bool(os.environ.get("LIVE_SERVER"))
+    if LIVE_SERVER:
+        STRIPE_API_KEY: str = os.environ.get("STRIPE_API_KEY_REAL")
+        STRIPE_WEBHOOK_SECRET: str = os.environ.get("STRIPE_WEBHOOK_SECRET_REAL")
+    else:
+        STRIPE_API_KEY: str = os.environ.get("STRIPE_API_KEY")
+        STRIPE_WEBHOOK_SECRET: str = os.environ.get("STRIPE_WEBHOOK_SECRET")
 
 
 
