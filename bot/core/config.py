fede9cb82774fbf40edefe96f4306439aacd14d4
Anatoly <anatoly.varkus@gmail.com>
2025-07-29 01:47:07 -0400

    asdads

--- bot/core/config.py
+++ bot/core/config.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
 from dotenv import load_dotenv
+from typing import List
 import os
 load_dotenv()
 
 class Settings(BaseModel):
-    TG_TOKEN =  os.getenv("TG_TOKEN")
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    ALLOWED_USER_IDS = [280963071, 85450372, 322850775, 224737994, 149768]
+    TG_TOKEN: str =  os.getenv("TG_TOKEN")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    ALLOWED_USER_IDS: List = [280963071, 85450372, 322850775, 224737994, 149768]
 
 settings = Settings()
