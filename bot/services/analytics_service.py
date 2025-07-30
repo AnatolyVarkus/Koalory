cc1bfbaaf7d6973daecb4f8b2e134e36cbe8cfff
Anatoly <anatoly.varkus@gmail.com>
2025-07-29 20:30:16 -0400

    asasd

--- bot/services/analytics_service.py
+++ bot/services/analytics_service.py
@@ -30,7 +30,7 @@ async def gather_analytics(session: AsyncSession) -> str:
 
     # PAYMENTS
     total_payments = await session.scalar(
-        select(func.count(PaymentsModel.amount_in_cents))
+        select(func.sum(PaymentsModel.amount_in_cents))
     )
     total_paid_users = await session.scalar(
         select(func.count(func.distinct(PaymentsModel.user_id)))
@@ -56,8 +56,8 @@ async def gather_analytics(session: AsyncSession) -> str:
 • Avg. per user: {avg_stories_per_user:.2f}
 
 💰 <b>Payments</b>
-• Total payments: ${total_payments:.2f}
+• Total payments: ${total_payments/100:.2f}
 • Unique payers: {total_paid_users}
-• Avg. per payer: ${avg_payments_per_user:.2f}
+• Avg. per payer: ${avg_payments_per_user/100:.2f}
 • Total story credits sold: {total_payment_sum or 0}
 """.strip()
